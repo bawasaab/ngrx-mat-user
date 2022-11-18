10de8c9802a59bea4b392e2e19a9e8e6478eaae6
Deepak Bawa <deepak.bawa@orionesolutions.in>
2022-11-18 16:48:37 +0530

    sample counter state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { reducers, metaReducers } from './store';
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    MyCounterComponent
   ],
   imports: [
     BrowserModule,
@@ -24,8 +28,10 @@ import { environment } from '../environments/environment';
     BrowserAnimationsModule,
     NgMatModule,
     ReactiveFormsModule,
-    StoreModule.forRoot({}, {}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    // StoreModule.forRoot(reducers, { metaReducers }),
+    StoreModule.forRoot({ count: counterReducer }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
